9bb9c0de45d49266c09ab216a5f6eee5682527db
Ahmed Aly <aaoflaca@gmail.com>
2020-01-08 13:44:39 -0800

    replace colors with chalk for coloring

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const chalk = require('chalk');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
@@ -81,14 +82,20 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
+  // console.log(
+  //   `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  // )
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    chalk.yellow.bold.bgBlue(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
   )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
+  // console.log(`Error: ${err.message}`.red);
+  console.log(chalk.whiteBright.bold.bgRedBright(`Error: ${err.message}`));
   // Close server & exit process
   // server.close(() => process.exit(1));
 });
